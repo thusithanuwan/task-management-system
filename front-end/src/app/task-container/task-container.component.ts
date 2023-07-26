03ddeba0a8bc00cd7cc6f15d6fb384828d2b761b
thusitha <nuwan12403482@gmail.com>
2023-07-26 20:25:22 +0530

    Inject HttpClient & implemented saveTask()

--- front-end/src/app/task-container/task-container.component.ts
+++ front-end/src/app/task-container/task-container.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Task} from "../dto/task";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-task-container',
@@ -7,7 +9,24 @@ import { Component } from '@angular/core';
 })
 export class TaskContainerComponent {
 
-  saveTask(txt: HTMLInputElement) {
+  taskList: Array<Task> = [];
 
+  constructor(private http:HttpClient) {
+    http.get<Array<Task>>('http://localhost:8080/app/api/v1/task')
+      .subscribe(taskList => this.taskList = taskList );
+
+  }
+
+  saveTask(txt: HTMLInputElement) {
+    if (!txt.value.trim()) {
+      txt.select();
+      return;
+    }
+    this.http.post<Task>('http://localhost:8080/app/api/v1/task',
+      new Task(0,txt.value,"NOT_COMPLETED")).subscribe(task =>{
+      this.taskList.push(task);
+      txt.value='';
+      txt.focus();
+    });
   }
 }
