b741c1f03a0f970b54123fc0b476c0204d39ee1a
thusitha <nuwan12403482@gmail.com>
2023-07-26 22:00:24 +0530

    import router

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,10 +1,11 @@
 import express ,{json} from 'express';
 import cors from 'cors';
+import {router} from "./api/task-controller";
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 
-app.use("/app/api/v1/task");
+app.use("/app/api/v1/task",router);
 app.listen(8080,()=>{console.log("Server has been started at : 8080")});
